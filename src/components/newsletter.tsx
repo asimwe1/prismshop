1d8444b44f37e92a789c1bf1ce142b3579a32614
asimwe1 <landryasimwe@gmail.com>
2024-06-16 16:08:23 +0200

    input field added

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -31,11 +31,13 @@ const Newsletter: NextPage<NewsletterType> = ({ className = "" }) => {
             <div className="flex-1 shadow-[0px_0px_70px_rgba(0,_0,_0,_0.25)] rounded-mini bg-white flex flex-row items-start justify-between pt-[1rem] pb-[0.937rem] pr-[1.25rem] pl-[2.687rem] box-border max-w-full gap-[1.25rem] z-[1] mq1300:flex-wrap mq1300:pl-[1.313rem] mq1300:box-border">
               <div className="h-[8.438rem] w-[61.313rem] relative shadow-[0px_0px_70px_rgba(0,_0,_0,_0.25)] rounded-mini bg-white hidden max-w-full" />
               <div className="w-[33.063rem] flex flex-col items-start justify-start pt-[1.187rem] px-[0rem] pb-[0rem] box-border max-w-full">
-                <h2 className="m-0 self-stretch h-[3.625rem] relative text-inherit font-light font-inherit flex items-center shrink-0 z-[2] mq900:text-[1.5rem] mq450:text-[1.125rem]">
-                  Enter your email address
-                </h2>
-              </div>
-              <button className="cursor-pointer [border:none] py-[1.687rem] pr-[3.5rem] pl-[3.687rem] bg-blue h-[6.5rem] rounded-3xs flex flex-row items-start justify-start box-border min-w-[15.563rem] z-[2] hover:bg-mediumpurple-100 mq1300:flex-1">
+    <input
+      type="email"
+      placeholder="Enter your email address"
+      className="m-0 self-stretch h-[3.625rem] relative text-inherit font-light font-inherit flex items-center shrink-0 z-[2] mq900:text-[1.5rem] mq450:text-[1.125rem] p-[0.5rem]"
+    />
+  </div>              
+  <button className="cursor-pointer [border:none] py-[1.687rem] pr-[3.5rem] pl-[3.687rem] bg-blue h-[6.5rem] rounded-3xs flex flex-row items-start justify-start box-border min-w-[15.563rem] z-[2] hover:bg-mediumpurple-100 mq1300:flex-1">
                 <div className="h-[6.5rem] w-[15.563rem] relative rounded-3xs bg-blue hidden" />
                 <div className="relative text-[1.875rem] capitalize font-extrabold font-lato text-white text-left z-[3] mq900:text-[1.5rem] mq450:text-[1.125rem]">
                   subscribe
